96a7d1ed35ead827a1ff913fa5950b04f5cc9dbf
Jeremiah <jjessel@jcubedapps.com>
2015-05-27 19:55:32 -0700

    Initial Commit

---
+++ AssignmentStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AssignmentStarter
+//
+//  Created by Jeremiah Jessel on 5/27/15.
+//  Copyright (c) 2015 Learn Core Data. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
